32baeebb92709b5404332e8f7e5bfaf97f5d3003
Petyulino <fogarasipeti@gmail.com>
2025-01-05 17:44:09 +0100

    favicon mod index.html

---
+++ src/assets/manifest.js
@@ -0,0 +1,20 @@
+{
+  "short_name": "Otthon",
+  "name": "Otthonkorszerűsítés Kalkulátor",
+  "icons": [
+    {
+      "src": "./assets/logo/favicon2-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "./assets/logo/favicon2-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ],
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#00796b"
+}
